e819d8c7533209ed67a752fa400f6201fafeacfe
Rone Silveira <ronebs10@hotmail.com>
2023-01-18 14:35:03 -0300

    update package.json file

--- react.js
+++ react.js
@@ -1,44 +1,44 @@
 module.exports = {
-env: {
-  browser: true,
-  es2021: true
+  env: {
+    browser: true,
+    es2021: true
   },
-  settings: {
-    react: {
-    version: "detect"
-    }
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    settings: {
+      react: {
+      version: "detect"
+      }
     },
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
-  plugins: [
-    "react",
-    "react-hooks",
-    "@typescript-eslint"
-  ],
-  rules: {
-    'prettier/prettier': ["error", {
-      'singleQuote': true,
-      'trailingComma': 'none',
-      'semi': false,
-    }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off"
-  }
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:prettier/recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    plugins: [
+      "react",
+      "react-hooks",
+      "@typescript-eslint"
+    ],
+    rules: {
+      'prettier/prettier': ["error", {
+        'singleQuote': true,
+        'trailingComma': 'none',
+        'semi': false,
+      }],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off"
+    }
 }
   
